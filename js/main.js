32c329fb792e7731c7b2afb920f9f0fe64a5c777
Tesla7D <grikzyrev@gmail.com>
2015-03-23 22:52:22 -0400

    loginbox style change
    just made loginbox more visible.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(window).load(function(){
 	$("#loginTitle").delay(1000).animate({ opacity: 1 },1000,"easeOutQuart",function(){
 		setTimeout(function(){
-			$({ progress:0 }).stop().animate({ progress: 1 },{
+			$({ progress:0 }).stop().animate({ progress: 2 },{
 				 duration:3000
 				,easing:"easeOutQuart"
 				,step:function(progress){
 					console.log(progress);
-					$("#loginBox").css("background-color","rgba(0,0,0," + progress*0.05 + ")");
-					$("#loginBox").css("border","1px solid rgba(0,0,0," + progress*0.1 + ")");
+					$("#loginBox").css("background-color","rgba(0,0,0," + progress*0.1 + ")");
+					$("#loginBox").css("border","1px solid rgba(0,0,0," + progress*0.05 + ")");
 					$("#loginContainer").css("opacity",progress);
 				}
 				,complete:function(){
