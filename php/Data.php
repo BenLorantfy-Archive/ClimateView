3589f25eb86fc084b8338d470b66ed84c17d2e46
Ben <ben@Bens-MacBook-Pro-2.local>
2015-04-22 01:10:58 -0400

    lots of front end changes, csv file upload, html5 navigation

--- php/Data.php
+++ php/Data.php
@@ -4,4 +4,14 @@ class Data{
 		// automatically converted to json by Router
 		return array();
 	}
+	
+	function uploadUserData($request){
+		$csvPath = $_FILES["dataUploadInput"]["tmp_name"];
+		
+		$csv = fopen($csvPath,"r");
+		$a = fgetcsv($csv);
+		fclose($csv);
+		
+		return $a;
+	}
 }
